5b00b1c2b6c134aca5e8387e65fcabec39d43957
pandero7 <rafasoli10@gmail.com>
2023-10-29 19:51:40 -0300

    formatting

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -1,8 +1,8 @@
-export interface Props{
+export interface Props {
   copywrite: string;
 }
 
-export default function Footer({copywrite}: Props) {
+export default function Footer({ copywrite }: Props) {
   return (
     <div class="lg:container py-4 mx-8 md:mx-16 lg:mx-auto flex justify-center items-center gap-2">
       <span>{copywrite}</span>
